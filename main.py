dcb45cfdaa25de1de4a887645048342cebf72343
tetianaklius <tetianaklius@gmail.com>
2023-01-10 22:15:39 +0200

    bot-test

--- main.py
+++ main.py
@@ -32,19 +32,20 @@ def message_text(message):
     bot.send_message(message.chat.id, "Python")
 
 
-@app.route("/" + TOKEN, methods=["POST"])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "Python Telegram Bot", 200
+bot.polling()
+
+# @app.route("/" + TOKEN, methods=["POST"])
+# def get_message():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return "Python Telegram Bot", 200
 
 
-@app.route("/")
-def main():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://test-bot-2-0.herokuapp.com/" + TOKEN)
-    return "Python Telegram Bot", 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+# @app.route("/")
+# def main():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://test-bot-2-0.herokuapp.com/" + TOKEN)
+#     return "Python Telegram Bot", 200
+#
+#
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
